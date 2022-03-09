f63e4739ca15228fa305645218317add039bbd12
Andres Yanquen <andresyanquen@hotmail.com>
2022-03-08 20:37:58 -0500

    High bugs for all the squads

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,43 @@ const router = Router();
 
 // Routes
 
-router.get('/secondtrigger', async(req, res) =>{
+
+
+router.get('/trigger-al-aa-ps-high', async(req, res) =>{
+
+    
+    var results = await getResults();
+    res.json(((results)))
+    dataTemplate = {
+        algorithmsSquad: 0,
+        applicantsAcquisitionSquad:0,
+        platformSquad:0
+    }
+})
+
+router.get('/trigger-g-ugg-w-high', async(req, res) =>{
 
     
     var results = await getResultsSecondList();
     res.json((JSON.stringify(results)))
+    dataSecondSquadList = {
+        genomeSquadDone: 0,
+        uggSquadDone:0,
+        workSquadDone:0
+    }
 })
+
+router.get('/trigger-t-tsas-high', async(req, res) =>{
+
+    
+    var results = await getResultsThirdList();
+    res.json((JSON.stringify(results)))
+    dataThirdSquadList = {
+        talentSquadDone: 0,
+        talentSeekerAcquisitionSquad:0
+    }
+})
+
 
 router.get('/changestate/:text', async(req, res)=>{
     var dataSet= {
@@ -19,7 +50,7 @@ router.get('/changestate/:text', async(req, res)=>{
     console.log(req.params['text']);
     dataSet['data'] = req.params['text'];
     console.log(dataSet)
-    fs.writeFile(path.join(__dirname, '../JSONFile/data.json'), JSON.stringify(dataSet), function(err){
+    fs.writeFile(path.join(__dirname, '../JSONFile/data_AL_AA_PS.json'), JSON.stringify(dataSet), function(err){
     
 });
 
@@ -27,26 +58,42 @@ router.get('/changestate/:text', async(req, res)=>{
 })
 
 
-router.get('/trigger', async(req, res) =>{
 
-    
-    var results = await getResults();
-    res.json((JSON.stringify(results)))
-})
 
-router.get('/data', async(req, res)=>{
+router.get('/data-al-aa-ps-high', async(req, res)=>{
     let dataSquad;
-    fs.readFile(path.join(__dirname, '../JSONFile/data.json'),{encoding:'utf-8'} , (err, jsonString) =>{
+    fs.readFile(path.join(__dirname, '../JSONFile/data_AL_AA_PS.json'),{encoding:'utf-8'} , (err, jsonString) =>{
         if(err){
         }else{
             dataSquad=jsonString;     
             res.json(JSON.parse(dataSquad));
         }
     })
-
-
-
 })
+
+router.get('/data-g-ugg-w-high', async(req, res)=>{
+    let dataSquad;
+    fs.readFile(path.join(__dirname, '../JSONFile/data_G_UGG.json'),{encoding:'utf-8'} , (err, jsonString) =>{
+        if(err){
+        }else{
+            dataSquad=jsonString;     
+            res.json(JSON.parse(dataSquad));
+        }
+    })
+})
+
+router.get('/data-t-tsas-high', async(req, res)=>{
+    let dataSquad;
+    fs.readFile(path.join(__dirname, '../JSONFile/data_T_TSAS.json'),{encoding:'utf-8'} , (err, jsonString) =>{
+        if(err){
+        }else{
+            dataSquad=jsonString;     
+            res.json(JSON.parse(dataSquad));
+        }
+    })
+})
+
+
 
 ///Code
 
@@ -56,16 +103,28 @@ const { json } = require('express/lib/response')
 
 const apiURL = "https://api.notion.com/v1/databases/04b356ab699543a7824fef7294344e5b/query";
 
-const dataTemplate = {
+var dataTemplate = {
     algorithmsSquad: 0,
     applicantsAcquisitionSquad:0,
     platformSquad:0
 }
 
+var dataSecondSquadList = {
+    genomeSquadDone: 0,
+    uggSquadDone:0,
+    workSquadDone:0
+}
+
+var dataThirdSquadList = {
+    talentSquadDone: 0,
+    talentSeekerAcquisitionSquad:0
+}
+
 
 
 const listSquads = ['Algorithms squad','Applicants acquisition squad','Platform squad']/* , 'Genome squad','Work squad', 'Talent squad', 'UGG squad','Talent seeker acquisition squad'] */
-const secondListSquads = ['Genome squad','Work squad']/* , 'UGG squad','Talent seeker acquisition squad'] */
+const secondListSquads = ['Genome squad','UGG squad','Work squad']/* , ] */
+const thirdListSquads = ['Talent squad','Talent seeker acquisition squad']
 
 
 function request(body){
@@ -100,6 +159,16 @@ function body(next_cursor, has_more, squadBody, bugOrDone){
                     "multi_select": {
                         "contains": squadBody
                     }
+                },{
+                    "property":"Priority",
+                    "select":{
+                        "equals" : "Medium"
+                    }
+                },{
+                    "property":"Other labels",
+                        "multi_select":{
+                            "contains" : "Bug"
+                        }
                 }
             ]
         }
@@ -120,6 +189,16 @@ function body(next_cursor, has_more, squadBody, bugOrDone){
                     "multi_select": {
                         "contains": squadBody
                     }
+                },{
+                    "property":"Priority",
+                    "select":{
+                        "equals" : "Medium"
+                    }
+                },{
+                    "property":"Other labels",
+                        "multi_select":{
+                            "contains" : "Bug"
+                        }
                 }
             ]
         },
@@ -166,6 +245,7 @@ function trigger_hash_more(props){
         trigger = true
     } else {
         result_hash_more = false
+        next_cursor=''
     }
 }
 
@@ -183,11 +263,9 @@ let talentSeekerAcquisitionSquad=[];
 
 
 var SquadsDone = [algorithmsSquad, applicantsAcquisitionSquad, platformSquad]/* , genomeSquadDone, workSquadDone, talentSquadDone,uggSquadDone,talentSeekerAcquisitionSquad] */
-var SquadDoneSecondList = [genomeSquadDone, workSquadDone]/* ,uggSquadDone,talentSeekerAcquisitionSquad]
- */
+var SquadDoneSecondList = [genomeSquadDone, uggSquadDone,workSquadDone]/* ,workSquadDone,talentSeekerAcquisitionSquad] */
+var SquadDoneThirdList = [talentSquadDone, talentSeekerAcquisitionSquad]
 const getResults = async() =>{
-    var counter_result=0;
-
 
     for (let i = 0; i < listSquads.length; i++) {
         trigger_hash_more(true);
@@ -198,6 +276,8 @@ const getResults = async() =>{
             }
             result = await requestFunction(listSquads[i], done)
             SquadsDone[i] = [...SquadsDone[i], result.results.length]
+            console.log(SquadsDone)
+            console.log(next_cursor)
             /* counter_result=counter_result+result.results.length; */
 
     
@@ -205,6 +285,7 @@ const getResults = async() =>{
 
 
     }
+
 
     var counterone=0, countertwo=0, counterthree=0;
 
@@ -228,16 +309,21 @@ const getResults = async() =>{
     dataTemplate['applicantsAcquisitionSquad']= countertwo;
     dataTemplate['platformSquad']=counterthree;
     /* const obj = Object.assign({}, SquadsDone); */
-    fs.writeFile(path.join(__dirname, '../JSONFile/data.json'), JSON.stringify(dataTemplate), function(err){
+    fs.writeFile(path.join(__dirname, '../JSONFile/data_AL_AA_PS.json'), JSON.stringify(dataTemplate), function(err){
 
     });
 
+    algorithmsSquad=[];
+    applicantsAcquisitionSquad = []
+    platformSquad=[];
+    SquadsDone=[algorithmsSquad, applicantsAcquisitionSquad, platformSquad]
+    console.log(234, SquadsDone)
     return dataTemplate;
+
     
 }
 
 const getResultsSecondList = async() =>{
-    var counter_result=0;
 
 
     for (let i = 0; i < secondListSquads.length; i++) {
@@ -249,18 +335,105 @@ const getResultsSecondList = async() =>{
             }
             result = await requestFunction(secondListSquads[i], done)
             SquadDoneSecondList[i] = [...SquadDoneSecondList[i], result.results.length]
-            counter_result=counter_result+result.results.length;
+            console.log(SquadDoneSecondList)
+            console.log(next_cursor)
+            /* counter_result=counter_result+result.results.length; */
+
     
         }
+
+
     }
-    const obj = Object.assign({}, SquadDoneSecondList);
-    fs.writeFile(path.join(__dirname, '../JSONFile/data.json'), JSON.stringify(obj), function(err){
+
+
+    var counterone=0, countertwo=0, counterthree=0;
+
+    for(let i=0; i < SquadDoneSecondList[0].length; i++){
+        counterone = counterone + SquadDoneSecondList[0][i];
+    }
+
+    for(let i=0; i < SquadDoneSecondList[1].length; i++){
+        countertwo = countertwo + SquadDoneSecondList[1][i];
+    }
+
+    for(let i=0; i < SquadDoneSecondList[2].length; i++){
+        counterthree = counterthree + SquadDoneSecondList[2][i];
+    }
+    console.log(SquadDoneSecondList[0]);
+    console.log(SquadDoneSecondList[1]);
+    console.log(SquadDoneSecondList[2]);
+
+    dataSecondSquadList['genomeSquadDone'] = counterone;
+    dataSecondSquadList['uggSquadDone']= countertwo;
+    dataSecondSquadList['workSquadDone']=counterthree;
+    
+    /* const obj = Object.assign({}, SquadDoneSecondList); */
+    fs.writeFile(path.join(__dirname, '../JSONFile/data_G_UGG.json'), JSON.stringify(dataSecondSquadList), function(err){
 
     });
 
-    return SquadDoneSecondList;
+    genomeSquadDone=[];
+    uggSquadDone = []
+    workSquadDone=[];
+    SquadDoneSecondList=[genomeSquadDone, uggSquadDone, workSquadDone]
+    console.log(234, SquadDoneSecondList)
+    return dataSecondSquadList;
     
 }
+
+const getResultsThirdList = async() =>{
+
+
+    for (let i = 0; i < thirdListSquads.length; i++) {
+        trigger_hash_more(true);
+        while (result_hash_more != false) {
+            if (trigger) {
+                trigger_hash_more(false)
+                trigger = false
+            }
+            result = await requestFunction(thirdListSquads[i], done)
+            SquadDoneThirdList[i] = [...SquadDoneThirdList[i], result.results.length]
+            console.log(SquadDoneThirdList)
+            console.log(next_cursor)
+            /* counter_result=counter_result+result.results.length; */
+
+    
+        }
+
+
+    }
+
+
+    var counterone=0, countertwo=0;
+
+    for(let i=0; i < SquadDoneThirdList[0].length; i++){
+        counterone = counterone + SquadDoneThirdList[0][i];
+    }
+
+    for(let i=0; i < SquadDoneThirdList[1].length; i++){
+        countertwo = countertwo + SquadDoneThirdList[1][i];
+    }
+
+
+    console.log(SquadDoneThirdList[0]);
+    console.log(SquadDoneThirdList[1]);
+
+    dataThirdSquadList['talentSquadDone'] = counterone;
+    dataThirdSquadList['talentSeekerAcquisitionSquad']= countertwo;
+    
+    /* const obj = Object.assign({}, SquadDoneThirdList); */
+    fs.writeFile(path.join(__dirname, '../JSONFile/data_T_TSAS.json'), JSON.stringify(dataThirdSquadList), function(err){
+
+    });
+
+    talentSquadDone=[];
+    talentSeekerAcquisitionSquad = []
+    SquadDoneThirdList=[talentSquadDone, talentSeekerAcquisitionSquad]
+    console.log(234, SquadsDone)
+    return dataThirdSquadList;
+    
+}
+
 
 
 
